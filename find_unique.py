f38df6e8a5b6b63b1dbf63f237a9aab91ed5b38b
ubuntu <shamil.ishraq@gmail.com>
2014-09-07 13:06:35 -0400

    remove tabs

--- find_unique.py
+++ find_unique.py
@@ -3,34 +3,34 @@ import simplejson as json
 from multiprocessing import freeze_support
 
 def map_func(file):
-	with open(file, 'r') as file:
-		for line in file:
-			data = tuple(json.loads(line))
-			yield (data, 1)
+  with open(file, 'r') as file:
+    for line in file:
+      data = tuple(json.loads(line))
+      yield (data, 1)
 
 def reduce_func(data):
-	for d in data:
-		yield d[0]
+  for d in data:
+    yield d[0]
 
 def sort_key_provider(data):
-	return (data[2], data[3])
+  return (data[2], data[3])
 
 def main():
-	bldr = mr.JobBuilder()
-	bldr.set_name("Find unique")
-	bldr.set_mapper(map_func)
-	bldr.set_reducer(reduce_func)
-	bldr.set_input_dir("C:\/Users\/shamil\/Desktop\/capstone\/mr_test\/input")
-	bldr.set_output_dir("C:\/Users\/shamil\/Desktop\/capstone\/mr_test\/output")
-	bldr.set_mapper_count(4)
-	bldr.set_reducer_count(1)
-	bldr.set_sort_key_provider(sort_key_provider)
+  bldr = mr.JobBuilder()
+  bldr.set_name("Find unique")
+  bldr.set_mapper(map_func)
+  bldr.set_reducer(reduce_func)
+  bldr.set_input_dir("C:\/Users\/shamil\/Desktop\/capstone\/mr_test\/input")
+  bldr.set_output_dir("C:\/Users\/shamil\/Desktop\/capstone\/mr_test\/output")
+  bldr.set_mapper_count(4)
+  bldr.set_reducer_count(1)
+  bldr.set_sort_key_provider(sort_key_provider)
 
-	job = bldr.build()
+  job = bldr.build()
 
-	runner = mr.JobRunner()
-	r = runner.run(job)
+  runner = mr.JobRunner()
+  r = runner.run(job)
 
 if __name__ == '__main__':
-	freeze_support()
-	main()
+  freeze_support()
+  main()
--- flickr_search.py
+++ flickr_search.py
@@ -14,78 +14,78 @@ API = flkr.Photo()
 
 # Not meant to be thread-safe
 class Processor:
-	def __init__(self):
-		self.obj_count = 0
-		self.file = None
+  def __init__(self):
+    self.obj_count = 0
+    self.file = None
 
-	def write_to_file(self, data):
-		data = json.dumps(data)
-		file = self.get_file()
-		file.write(data + DELIMITER)
-		self.obj_count += 1
-		
-	def get_file(self):
-		if not self.file:
-			self.set_new_file()
-		elif self.obj_count == OBJS_PER_FILE:
-			self.close_file()
-			self.set_new_file()
-		return self.file
-	
-	def close_file(self):
-		self.file.flush()
-		self.file.close()
-		
-	def set_new_file(self):
-		filename = "%s/%s.txt" % (STORAGE_DIR, str(self.current_page))
-		print("Opening file: " + filename)
-		self.file = open(filename, 'w')
-		self.obj_count = 0
-	
-	def set_current_page(self, page_id):
-		self.current_page = page_id
-		
-	def ingest(self, data):
-		start = time()
-		self.set_current_page(data['photos']['page'])
-		print(self.current_page)
-		
-		for p in data['photos']['photo']:
-			try:
-				info = API.get_info({'photo_id' : p['id']})
-			except Exception as e:
-				print("ERROR retrieving photo", p)
-				print(e)
-				continue
-			new_photo = [p['id'], p['owner'], info['photo']['location']['latitude'], info['photo']['location']['longitude']]
-			self.write_to_file(new_photo)
-		
-		finish = time()
-		print("Time: ", finish - start)
-		
-		
+  def write_to_file(self, data):
+    data = json.dumps(data)
+    file = self.get_file()
+    file.write(data + DELIMITER)
+    self.obj_count += 1
+    
+  def get_file(self):
+    if not self.file:
+      self.set_new_file()
+    elif self.obj_count == OBJS_PER_FILE:
+      self.close_file()
+      self.set_new_file()
+    return self.file
+  
+  def close_file(self):
+    self.file.flush()
+    self.file.close()
+    
+  def set_new_file(self):
+    filename = "%s/%s.txt" % (STORAGE_DIR, str(self.current_page))
+    print("Opening file: " + filename)
+    self.file = open(filename, 'w')
+    self.obj_count = 0
+  
+  def set_current_page(self, page_id):
+    self.current_page = page_id
+    
+  def ingest(self, data):
+    start = time()
+    self.set_current_page(data['photos']['page'])
+    print(self.current_page)
+    
+    for p in data['photos']['photo']:
+      try:
+        info = API.get_info({'photo_id' : p['id']})
+      except Exception as e:
+        print("ERROR retrieving photo", p)
+        print(e)
+        continue
+      new_photo = [p['id'], p['owner'], info['photo']['location']['latitude'], info['photo']['location']['longitude']]
+      self.write_to_file(new_photo)
+    
+    finish = time()
+    print("Time: ", finish - start)
+    
+    
 def main():
-	params = { 'bbox' : '-79.396590,43.634020,-79.361485,43.675503', 'page' : PAGES['start'] }
-	data = API.search(params)
-	
-	pages = data['photos']['pages']
-	print("Total pages: ", pages)
-	
-	processor = Processor()
-	processor.ingest(data)
-	for p in range(PAGES['start'] + 1, PAGES['finish'] + 1):
-		params.update({ 'page' : p })
-		try:
-			data = API.search(params)
-			processor.ingest(data)
-		except Exception as e:
-			print("ERROR retrieving page: ", p)
-			print(e)
-			continue
-	processor.close_file()
-		
-	
-		
+  params = { 'bbox' : '-79.396590,43.634020,-79.361485,43.675503', 'page' : PAGES['start'] }
+  data = API.search(params)
+  
+  pages = data['photos']['pages']
+  print("Total pages: ", pages)
+  
+  processor = Processor()
+  processor.ingest(data)
+  for p in range(PAGES['start'] + 1, PAGES['finish'] + 1):
+    params.update({ 'page' : p })
+    try:
+      data = API.search(params)
+      processor.ingest(data)
+    except Exception as e:
+      print("ERROR retrieving page: ", p)
+      print(e)
+      continue
+  processor.close_file()
+    
+  
+    
 if __name__ == '__main__':
-	main()
-	
+  main()
+  
