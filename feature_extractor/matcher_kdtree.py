e9316153689c0374d80464988fb48ad5ea3109ef
Shamil Ishraq <shamil.ishraq@gmail.com>
2014-11-25 00:19:54 -0500

    filter before sorting

--- feature_extractor/matcher_kdtree.py
+++ feature_extractor/matcher_kdtree.py
@@ -89,9 +89,10 @@ def main():
   for i,(m,n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
       best_matches.append(matches[i])
-
-  best_matches = sorted(best_matches, key=lambda x: x[0].distance)
+      
+  # Filter by distance
   best_matches = [ m[0] for m in best_matches if m[0].distance < DISTANCE_LIMIT][:MATCHES_NUM]
+  best_matches = sorted(best_matches, key=lambda x: x[0].distance)
 
   print "Good mathces: ", len(best_matches)
   
