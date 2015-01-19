09d350136f2cd2c1669a4ca888fa5825de62fa17
Shamil Ishraq <shamil.ishraq@gmail.com>
2015-01-19 09:25:20 -0500

    save kmeans tree to db

--- indexer/kmeans_indexer.py
+++ indexer/kmeans_indexer.py
@@ -1,12 +1,15 @@
 import numpy as np
+import simplejson as json
 from sklearn import cluster
 from img_utils import get_imgs, get_features_np, get_features, convert_to_numpy
 from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 class KMeansNode():
   
   def __init__(self, data):
-    self.data = data # Flat list of all features, used for clustering
+    self.data = data 
     self.children = []
     self.parent = None
     self.count = 0 # Number of data points clustered for this node
@@ -14,9 +17,13 @@ class KMeansNode():
 
 class KMeansTree():
 
+  INSERT_NODE_SQL = text("INSERT INTO kmeans_tree (parent_id, descriptor, images, features) VALUES (:parent_id, :des, :images, :features);")
+  UPDATE_CHILDREN_SQL = text("UPDATE kmeans_tree SET children=:children where id=:id;")
+
   def __init__(self, branch):
-    self.data = []
+    self.data = [] # Flat list of all features, used for clustering
     self.root = None
+    self.db = None
     self.branch = branch
     self.imgs = [] # List of tuples ((img metadata), [features])
 
@@ -36,6 +43,12 @@ class KMeansTree():
     self.__set_parent(self.root.children, self.root)
     self.root.count = len(self.data)
 
+    self.__build_index_files()
+
+    fin = datetime.now()
+    print "Train time: ", (fin - start).seconds
+
+  def __build_index_files(self):
     print "Building index files"
     for img, features in self.imgs:
       for f in features:
@@ -44,11 +57,8 @@ class KMeansTree():
         n = nodes[0]
         d = self.__distance(f, n.data) 
         if d != 0: print d, img
-        n.imgs.append(img_id)
+        if img_id not in n.imgs: n.imgs.append(img_id)
 
-    fin = datetime.now()
-    print "Train time: ", (fin - start).seconds
-  
   def __cluster(self, data):
     #print "Cl len: ", len(data)
     #start = datetime.now()
@@ -123,7 +133,7 @@ class KMeansTree():
     else:
       k = 1
 
-    if not self.root: raise Exception("Tree is not built")
+    if not self.root: raise Exception("Tree has not been built")
 
     queue = [self.root]
     ret = []
@@ -145,6 +155,38 @@ class KMeansTree():
       k -= 1
       ret.append(node)
       self.__knn_search(data, queue, ret, k)
+
+  def save_to_db(self):
+    print "Saving to db"
+    s = datetime.now()
+    if not self.root: raise Exception("Tree has not been built")
+
+    self.__save_node(self.root, None)
+
+    f = datetime.now()
+    print "Time: ", (f - s).seconds
+
+  def __get_db(self):
+    if not self.db:
+      self.db = create_engine("mysql://root@localhost:3306/WIT")
+    return self.db
+  
+  def __save_node(self, node, parent_id):
+    """
+    Recursively inserts a node into db
+    Returns id of node
+    """
+    db = self.__get_db()
+    if type(node.data) == np.ndarray:
+      data = json.dumps(node.data.tolist())
+    else:
+      data = ''
+    row = db.execute(self.INSERT_NODE_SQL, parent_id=parent_id, des=data, images=json.dumps(node.imgs), features=node.count) 
+    node_id = row.lastrowid
+
+    children_ids = [ self.__save_node(c, node_id) for c in node.children ]
+    db.execute(self.UPDATE_CHILDREN_SQL, id=node_id, children=json.dumps(children_ids))
+    return node_id
 
 def build_index(branch_factor):
   imgs = get_imgs()
@@ -175,6 +217,7 @@ def main():
   tree = KMeansTree(20)
   tree.add_imgs(img_data)
   tree.train()
+  tree.save_to_db()
 
 if __name__ == '__main__':
   main()
