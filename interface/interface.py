d3af22ff1a90e38daf7fc46cc0bafedb913c5968
Shamil Ishraq <shamil.ishraq@gmail.com>
2015-01-19 14:43:32 -0500

    connecting interface with search

--- interface/interface.py
+++ interface/interface.py
@@ -1,18 +1,20 @@
-import os
-import cv2
-import numpy
+import os, sys
+sys.path.append(os.path.join(os.getcwd(), "../matcher"))
+from search import SearchService
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-from matcher import create_repo_and_match_image, Descriptor_Type
 app = Flask(__name__)
+
+search_service = SearchService()
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        file.save(os.path.join('C:\Code\DesignProject', file.filename))
-        input_img = cv2.imread(file.filename)
-        best_match = create_repo_and_match_image(input_img, Descriptor_Type.ORB)
-        return redirect(url_for('uploaded_file',  match=best_match))
+        file.save(os.path.join(os.getcwd() +'/imgs/', file.filename))
+        matches = search_service.search(os.path.join(os.getcwd() + '/imgs/', file.filename))
+        coordinates = [ list(m.location)  for m in matches ]
+        print coordinates
+        return redirect(url_for('uploaded_file', coordinates=coordinates))
     return '''
     <!doctype html>
         <title>Where is this? Photo Location Finder</title>
@@ -23,10 +25,10 @@ def upload_file():
         </form>
     '''
 	
-@app.route('/uploads/<match>')
-def uploaded_file(match):
-    coordinateList = [[43.6592, -79.3935 ]] #[43.6584115,-79.3976985], [43.6592653, -79.3959926]]
-    return render_template('drawmaps.html', coordinateList = coordinateList)#send_from_directory('C:\Code\DesignProject\Test\database',match)
+@app.route('/uploads/<coordinates>')
+def uploaded_file(coordinates):
+    coordinateList = coordinates #[43.6584115,-79.3976985], [43.6592653, -79.3959926]]
+    return render_template('drawmap.html', coordinateList = coordinateList)#send_from_directory('C:\Code\DesignProject\Test\database',match)
 
 if __name__ == '__main__':
     app.run(debug=True)
