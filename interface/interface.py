14433e9b2493d8d12f64afb896d710177d17fbaa
camilla11 <camilla.tabis@gmail.com>
2015-03-06 18:43:04 -0500

    Interface changes

--- interface/interface.py
+++ interface/interface.py
@@ -20,12 +20,63 @@ def upload_file():
         return redirect(url_for('uploaded_file', coordinates=coordinates))
     return '''
     <!doctype html>
+    <link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/gen-light" rel="stylesheet" type="text/css"/> 
+        <body>
         <title>Where is this? Photo Location Finder</title>
-        <h1>Upload a photo to retrieve the coordinates.</h1>
-        <form action="" method=post enctype=multipart/form-data>
-            <p><input type=file name=file>
-            <input type=submit value=Upload>
-        </form>
+        <br><br><br><br>
+        <div id="header">
+        Where is this? Photo Location Finder
+        </div>
+        <div id="section">
+        <h3>Upload a photo to retrieve the coordinates.</h3>
+        <form action="" method=post enctype=multipart/form-data >
+            <input type=file name=file ">
+            <input type=submit value=Upload ">
+        </form><br>
+        </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div id="footer">
+        Camilla Tabis and Shamil Ishraq
+        </div>
+        </body>
+        <style>
+            @font-face {
+                font-family: myFirstFont;
+                src: bb.otf;
+            }
+
+            #header {
+                background-color:black;
+                color:white;
+                width:500px;
+                margin: 0 auto;
+                text-align:center;
+                font-family: 'GenW01Light'; 
+                padding:20px;
+            }
+            #nav {
+                line-height:30px;
+                background-color:#eeeeee;
+                height:300px;
+                width:100px;
+                float:left;
+                padding:5px;
+            }
+            #section {
+                background-color:#DEEFFF;
+                width:500px;
+                margin: 0 auto;
+                padding:20px;
+                text-align:center;
+                font-family: 'GenW01Light'; 
+            }
+            #footer {
+                float: right;
+
+                font-family: 'GenW01Light'; 
+                text-align: right;
+                width: 259px;
+            }
+        </style>    
     '''
 	
 @app.route('/uploads/<coordinates>')
